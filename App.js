0c36c4fa806da5b817af36c436dbf54bacf6c285
YoanSinH <joancifuentes@outlook.es>
2022-08-28 21:46:47 -0500

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
+  const [image, setImage] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+    console.log(image.file)
+  },[]);
+
+  const fetchData = () => {
+    fetch("https://coffee.alexflipnote.dev/random.json")
+    .then(response => response.json())
+    .then(jsonResponse => setImage(jsonResponse))
+    .catch(error => console.log(error))
+    console.log("a")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Random Coffee Image</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: image.file,
+        }}
+      />
+
+      <Button onPress={fetchData} title="Next"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +40,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1,
+    resizeMode: 'contain',
   },
 });
---
+++ PRIVACY.md
@@ -0,0 +1,7 @@
+## Política de privacidad
+
+COFFEE APP no recopila ningun tipo de dato personal.
+
+COFFEE APP no recopila ningún análisis que no sean los análisis que son recopilados por terceros como la tienda de Google Play de forma predeterminada, o la API de consumo de alexflipnote.
+
+Esta política de privacidad está sujeta a cambios, pero los siguientes términos siempre estarán en vigor: COFFEE APP NUNCA enviará ninguno de sus datos al titular o de cualquier tercero, sin su consentimiento informado y explícito.
